db3d2e391c9dd602fd92cd0de2b0d1e776baf247
masukatto7600 <hikaru_24s1102548@nnn.ed.jp>
2024-07-10 12:06:47 +0900

    安倍晋三

--- index.js
+++ index.js
@@ -11,28 +11,43 @@ const server = http
 
     switch (req.method) {
       case 'GET':
-        if (req.url === '/enquetes/yaki-tofu') {
+        if (req.url === '/') {
+          res.write('<!DOCTYPE html><html lang="ja"><body>' +
+            '<h1>アンケートフォーム</h1>' +
+            '<a href="/enquetes">アンケート一覧</a>' +
+            '</body></html>');
+        } else if (req.url === '/enquetes') {
+          res.write('<!DOCTYPE html><html lang="ja"><body>' +
+            '<h1>アンケート一覧</h1><ul>' +
+            '<li><a href="/enquetes/abe-ymgm">安倍晋三 or 山上徹也？</a></li>' +
+            '<li><a href="/enquetes/jap-chong">どちらが死滅するべきですか？</a></li>' +
+            '<li><a href="/enquetes/zozei-kento">岸田と言えば？</a></li>' +
+            '</ul></body></html>');
+        } else if (req.url === '/enquetes/abe-ymgm') {
           res.write(
             pug.renderFile('./form.pug', {
               path: req.url,
-              firstItem: '焼き肉',
-              secondItem: '湯豆腐'
+              title: '安倍晋三 or 山上徹也？',
+              firstItem: '安倍晋三',
+              secondItem: '山上徹也'
             })
           );
-        } else if (req.url === '/enquetes/rice-bread') {
+        } else if (req.url === '/enquetes/jap-chong') {
           res.write(
             pug.renderFile('./form.pug', {
               path: req.url,
-              firstItem: 'ごはん',
-              secondItem: 'パン'
+              title: 'どちらが死滅するべきですか？',
+              firstItem: 'ジャップ',
+              secondItem: 'チョン'
             })
           );
-        } else if (req.url === '/enquetes/sushi-pizza') {
+        } else if (req.url === '/enquetes/zozei-kento') {
           res.write(
             pug.renderFile('./form.pug', {
               path: req.url,
-              firstItem: '寿司',
-              secondItem: 'ピザ'
+              title: '岸田と言えば？',
+              firstItem: '増税メガネ',
+              secondItem: '検討士'
             })
           );
         }
@@ -64,7 +79,7 @@ const server = http
   .on('clientError', e => {
     console.error(`[${new Date()}] Client Error`, e);
   });
-const port = 8000;
+const port = process.env.PORT || 8000;
 server.listen(port, () => {
   console.info(`[${new Date()}] Listening on ${port}`);
 });
